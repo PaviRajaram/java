bd9b9543b4cb72f1b9ed6cfe33fe4a7c42623aa9
PaviRajaram <121500256+PaviRajaram@users.noreply.github.com>
2023-02-08 10:18:16 +0530

    Add files via upload

--- book.java
+++ book.java
@@ -1,16 +1,28 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class book {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int c;
+        List<memberReg> list=new ArrayList<>();
+        Integer n;
+        memberReg member1=new memberReg();
+            System.out.println("Enter your Registration number");
+            n= sc.nextInt();
+            member1.setRegNum(n);
+            list.add(member1);
+        int c,m;
         do {
-            memberReg member1=new memberReg();
             theBooks theBook=new theBooks();
             theBook.displayMenu();
             System.out.println("Enter your choice");
             c= sc.nextInt();
+            System.out.println("Enter your Registration Number");
+            m= sc.nextInt();
+            int v;
+            v=m-n;
+            memberReg obj=list.get(v);
             switch (c){
                 case 1:
                 {
@@ -20,16 +32,14 @@ public class book {
                     System.out.println("Enter your Name");
                     member1.setName(sc.next());
                     System.out.println("Enter your Registration Number");
-                    member1.setRegNum(sc.next());
-                    System.out.println("Your name :"+member1.getName()+"Your Id"+member1.getRegNum());
+                    member1.setRegNum(sc.nextInt());
+                    System.out.println("Your name :"+member1.getName()+"  "+"Your Id"+member1.getRegNum());
                 }break;
                 case 3:{
-                    checkIn checkInObj=new checkIn();
-                    checkInObj.checkInBook();
+                    obj.checkInBook();
                 }break;
                 case 4:{
-                    checkIn checkInObj=new checkIn();
-                    checkInObj.checkOutBook();
+                    obj.checkOutBook();
                 }break;
                 default:
                     System.out.println("PRESS 0 TO EXIT");
@@ -38,18 +48,13 @@ public class book {
         }while(c!=0);
         }
 }
-class checkIn{
-    String regNum,bookName;
+class checkIn extends theBooks{
+    String bookName;
     Scanner sc=new Scanner(System.in);
     theBooks theBook=new theBooks();
     void checkInBook(){
-        memberReg MR=new memberReg();
-        System.out.println("Enter Registration Number");
-        MR.setRegNum(sc.next());
-        theBook.addMember();
             System.out.println("Enter book name");
             bookName=sc.next();
-            if (MR.list.contains(regNum)) {
                 if (theBook.books.contains(bookName)) {
                     System.out.println("Book available,return on date");
                     theBook.books.remove(bookName);
@@ -60,18 +65,11 @@ class checkIn{
                 } else {
                     System.out.println("Not available");
                 }
-            }else {
-                System.out.println("Registration number not found");
-            }
+
     }
     void checkOutBook(){
-        memberReg MR=new memberReg();
-        System.out.println("Enter Registration Number");
-        MR.setRegNum(sc.next());
-        theBook.addMember();
         System.out.println("Enter book name");
         bookName=sc.next();
-        if (MR.list.contains(regNum)) {
             if (theBook.books.contains(bookName)) {
                 System.out.println("Book not matched");
             } else {
@@ -80,53 +78,45 @@ class checkIn{
                 for (String book : theBook.books) {
                     System.out.println(book);
                 }
-            }
-        }else {
-            System.out.println("Registration number not found");
         }
     }
 }
-class memberReg{
-    ArrayList<String> list=new ArrayList<>();
-    String name ,regNum;
+class memberReg extends checkIn{
+    String name ;
+    int regNum;
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public String getRegNum() {
+    public int getRegNum() {
         return regNum;
     }
-    public void setRegNum(String regNum) {
+    public void setRegNum(int regNum) {
         this.regNum = regNum;
-        for(int i=0;i<3;i++){
-            memberReg mr=new memberReg();
-            list.add(mr.regNum);
-        }
     }
 }
 class theBooks{
+    ArrayList<String> books = new ArrayList<>();
     void displayMenu(){
         System.out.println("press 0 to Exit");
         System.out.println("press 1 to checkAvailability");
         System.out.println("Press 2 to Register");
         System.out.println("Press 3 to CheckIn");
         System.out.println("Press 4 to CheckOut");
-    }
-    ArrayList<String> books = new ArrayList<>();
-    theBooks() {
         books.add("b1");
         books.add("b2");
         books.add("b3");
         books.add("b4");
     }
-    ArrayList<String> list=new ArrayList<>();
+
+    /*ArrayList<Integer> list=new ArrayList<>();
     void addMember(){
         for(int i=0;i<3;i++){
             memberReg mr=new memberReg();
             list.add(mr.regNum);
-        }
-    }
+        }*/
+
 }
 
